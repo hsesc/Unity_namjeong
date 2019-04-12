c3e6ad9b416fce3fe2440761f9c31479c9edd695
KimNamJeong <skawjd311@naver.com>
2019-04-12 15:20:44 +0900

    0412

---
+++ Assets/0412/Assets/Unity_jieun-master/Unity_jieun-master/Assets/Script/TimerGameoverLogic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerGameoverLogic : MonoBehaviour {
+
+    int countDownStartValue = 5;
+    public Text timerUI;
+
+    // Use this for initialization
+    void Start () {
+        countDownTimer();
+        
+
+    }
+	void countDownTimer()
+    {
+        if(countDownStartValue > 0)
+        {
+            timerUI.text = "Timer : " + countDownStartValue;
+            countDownStartValue--;
+            Invoke("countDownTimer", 1.0f);
+        }
+        else
+        {
+            timerUI.text = "GameOver!!";
+           
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
