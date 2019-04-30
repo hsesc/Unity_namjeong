014aeb58477c76dab0db997792d6ec2eee99a464
KimNamJeong <skawjd311@naver.com>
2019-04-30 18:29:54 +0900

    0430_2

---
+++ Assets/0430_F/Assets/Dnk_Dev/HospitalHorrorPack/Script/LightSwitch.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightSwitch : MonoBehaviour
+{
+    public GameObject l_light;              //불빛
+
+    private bool onTrigger; //범위에 들어가서 에임을 맞추었는지 아닌지
+    private bool lightOn;                   //불이 켜졌는지 아닌지
+    private ActionController playerHand;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            playerHand = GameObject.FindWithTag("MainCamera").GetComponent<ActionController>();
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.CompareTag("Player"))      //ComparTag 가 속도면에서 gameObject.tag 보다 나은것 같다
+        {
+            if (playerHand.hitinfo2.transform == transform.GetChild(0)) // 에임이 물체에 있는 상태에서
+            {
+                if (OVRInput.GetDown(OVRInput.Button.One)) //키 누르면
+                {
+                    onTrigger = true; //범위에 들어갔는지 아닌지 판별
+                }
+
+                TryEvent();
+
+                if (lightOn) //불빛 활성화되어 있으면
+                {
+                    playerHand.SetText("불 끄려면 <color=yellow>(F)</color>"); //물체 텍스트 설정
+                }
+                else//비활성화 되어 있으면
+                {
+                    playerHand.SetText("불 키려면 <color=yellow>(F)</color>");
+                }
+            }
+            else
+            {
+                playerHand.SetText("");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            playerHand.SetText("");
+            playerHand = null;
+        }
+    }
+
+    private void TryEvent()
+    {
+        if (onTrigger == true)   //F 누르면(1번 실행)
+        {
+            onTrigger = false;
+
+            lightOn = !lightOn;             //불빛 활성화/비활성화
+        }
+    }
+
+    void Start()
+    {
+        l_light.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (lightOn)
+        {
+            l_light.SetActive(true);
+        }
+        else
+        {
+            l_light.SetActive(false);
+        }
+    }
+}
---
+++ Assets/0430_F/Assets/Dnk_Dev/HospitalHorrorPack/Script/RefDoorOpen.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefDoorOpen : MonoBehaviour {
+
+    private bool refDoorOpen; //냉장고 열렸는지 아닌지
+
+    private bool onTrigger; //범위에 들어가서 에임을 맞추었는지 아닌지
+    private ActionController playerHand;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            playerHand = GameObject.FindWithTag("MainCamera").GetComponent<ActionController>();
+            //playerHand.SetText("살펴보기 <color=yellow>(F)</color>");
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.CompareTag("Player"))      //ComparTag 가 속도면에서 gameObject.tag 보다 나은것 같다
+        {
+            if (playerHand.hitinfo2.transform == transform.GetChild(0)) // 에임이 물체에 있는 상태에서
+            {
+                if (Input.GetKeyDown(KeyCode.F)) //키 누르면
+                {
+                    onTrigger = true; //범위에 들어갔는지 아닌지 판별
+                }
+
+                TryEvent();
+            }
+            else
+            {
+                //playerHand.SetText("");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            //playerHand.SetText("");
+            playerHand = null;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (refDoorOpen)
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot;
+        }
+        else
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 0.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot;
+        }
+    }
+
+    private void TryEvent()
+    {
+        if (onTrigger == true)   //F 누르면(1번 실행)
+        {
+            onTrigger = false;
+
+            //playerHand.SetText("아무것도 없다");
+            refDoorOpen = !refDoorOpen;     //냉장고 열기 활성화/비활성화
+        }
+    }
+}
